a95dd5ba25527bf487d92737d137af27dcedc178
huy.tang2 <huy.tang2@netico.se>
2025-07-07 10:41:40 +0700

    feat: add custom login button with styling.

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,10 +1,19 @@
 <script>
 	import { Button } from '@/lib/components/ui/button';
+	import { WiseButton } from '@/lib/components/wise';
+	import { cn } from '@/lib/utils';
 	import { SignIn } from '@auth/sveltekit/components';
 </script>
 
-<SignIn provider="google" class="bg-red-300" options={{ redirectTo: '/' }}>
-	<Button slot="submitButton">
-		<span>Login</span>
-	</Button>
-</SignIn>
+<section
+	class={cn(
+		'flex h-screen w-screen flex-col items-center justify-center',
+		'bg-gradient-to-tr from-blue-500 to-purple-500'
+	)}
+>
+	<SignIn provider="google" class="" options={{ redirectTo: '/' }}>
+		<WiseButton variant="outline" slot="submitButton" class="bg-white text-black" size="lg">
+			<span>Login with Google</span>
+		</WiseButton>
+	</SignIn>
+</section>
